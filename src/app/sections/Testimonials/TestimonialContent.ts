639b6944d57abd29382c4e8bcb4599aafdcc7c82
Edmund Lin <elz.work22@gmail.com>
2024-12-14 17:51:11 +0800

    update: add 2nd testimonial

--- src/app/sections/Testimonials/TestimonialContent.ts
+++ src/app/sections/Testimonials/TestimonialContent.ts
@@ -3,20 +3,40 @@ const TestimonialContent = [
     profilePicture: "/images/zhenwei.jpg",
     English: {
       name: "Toh Zhen Wei",
-      title: "Fintech Undergraduate",
+      title: "SIT Fintech Undergraduate",
       headline:
         "Edmund consistently drives successful projects with innovation and leadership.",
     },
     Chinese: {
       name: "Toh Zhen Wei",
-      title: "金融科技本科生",
-      headline: "震铭总能以创新和领导力推动项目成功。",
+      title: "SIT 金融科技本科生",
+      headline: "震铭总能以创新能力和领导力推动项目成功。",
     },
     Malay: {
       name: "Toh Zhen Wei",
-      title: "Pelajar Sarjana Muda Fintech",
+      title: "Sarjana Muda Fintech SIT",
       headline:
         "Edmund sentiasa memacu projek berjaya dengan inovasi dan kepimpinan.",
+    },
+  },
+  {
+    profilePicture: "/images/yinphng.jpg",
+    English: {
+      name: "Lim Yin Phng",
+      title: "SMU ASEAN Scholar",
+      headline:
+        "Zhenming is an exceptional leader with strong strategic thinking and technological skills.",
+    },
+    Chinese: {
+      name: "林震铭",
+      title: "SMU ASEAN 奖学金得主",
+      headline: "震铭是一位具有卓越战略思维和技术能力的杰出领导者。",
+    },
+    Malay: {
+      name: "Lin Lin",
+      title: "Sarjana ASEAN SMU",
+      headline:
+        "Zhenming adalah seorang pemimpin yang cemerlang dengan pemikiran strategik yang kuat dan kemahiran teknologi yang hebat.",
     },
   },
 ];
