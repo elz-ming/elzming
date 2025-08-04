4335e69187f057b26f71badb28054d4a74c7f4d4
Edmund Lin <elz.zhenming@gmail.com>
2025-08-04 23:17:38 +0800

    initial push

---
+++ src/app/components/sections/Hero.tsx
@@ -0,0 +1,180 @@
+import Image from "next/image";
+
+export default function Hero() {
+  function StatBlock({
+    value,
+    label,
+  }: {
+    value: string | number;
+    label: string;
+  }) {
+    return (
+      <div
+        className="
+        md:h-16 2xl:h-20
+        flex flex-col justify-center items-center
+        md:items-end
+      "
+      >
+        <span
+          className="
+          font-semibold text-4xl
+          md:text-2xl 2xl:text-4xl
+        "
+        >
+          {value}
+        </span>
+        <span className="text-2xl md:text-lg 2xl:text-2xl">{label}</span>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className={`
+        w-full h-screen z-0 relative 
+        flex flex-col justify-start items-start 
+        md:flex-row md:justify-center
+        md:px-16 2xl:px-32
+      `}
+    >
+      {/* Left */}
+      <div
+        className={`
+          w-full order-2
+          md:w-[30%] md:h-full md:order-1
+          flex flex-col gap-2
+          md:gap-4 2xl:gap-8
+          px-8 py-4 
+          md:px-0 md:pb-0 md:pr-12 md:pt-[20vh] 2xl:pr-16
+        `}
+      >
+        {/* Hey, I'm Edmund */}
+        <div
+          className="
+          w-full 
+          md:h-16 2xl:h-20
+          md:flex md:items-center"
+        >
+          <span
+            className="
+            text-lg whitespace-nowrap
+            md:text-2xl
+            2xl:text-4xl
+            "
+          >
+            Hey, I'm Edmund
+          </span>
+        </div>
+
+        {/* A fintech expert */}
+        <div
+          className="
+          w-full
+          md:h-36 2xl:h-48
+          flex flex-row gap-1
+          md:items-center md:gap-4 
+          "
+        >
+          <span
+            className="
+            text-2xl whitespace-wrap font-[cursive]
+            md:text-4xl 2xl:text-6xl
+            "
+          >
+            FINANCIAL TECHNOLOGY EXPERT
+          </span>
+        </div>
+
+        {/* I design systems that scale, stories that stick, and solutions that matter. */}
+        <div
+          className="
+          w-full
+          md:h-16 2xl:h-20
+          md:flex md:items-center"
+        >
+          <span
+            className="
+            text-md whitespace-wrap
+            md:text-lg
+            2xl:text-2xl
+            "
+          >
+            I design systems that scale, stories that stick, and solutions that
+            matter.
+          </span>
+        </div>
+
+        {/* Buttons */}
+        <div
+          className="
+          hidden w-full
+          md:flex
+          flex flex-row 
+          md:gap-4 2xl:gap-8
+          md:text-lg 2xl:text-2xl
+          "
+        >
+          <button
+            className="
+            flex-1 bg-[#FEE575] 
+            py-2 rounded-md
+            font-bold
+            cursor-pointer hover:scale-105
+            "
+          >
+            Talk to Me
+          </button>
+          <button
+            className="
+            flex-1
+            border-3 border-[#FEE575] rounded-md
+            2xl:border-4
+            text-[#FEE575] font-bold
+            cursor-pointer hover:scale-110
+            "
+          >
+            Download CV
+          </button>
+        </div>
+      </div>
+
+      {/* Middle */}
+      <div
+        className={`
+          w-full h-[55%] bg-[#FEE575] overflow-hidden order-1 
+          md:w-[40%] md:h-[85%] md:order-2
+          2xl:h-[80%]
+          flex items-end
+          pt-[10vh] rounded-b-full 
+          md:pt-0
+        `}
+      >
+        <div
+          className="
+          relative w-full aspect-square
+          "
+        >
+          <Image src="/heroImage.jpg" className="object-contain" fill alt="" />
+        </div>
+      </div>
+
+      {/* Right */}
+      <div
+        className="
+        w-full order-3
+        md:w-[30%] md:h-full
+        grid grid-rows-2 grid-cols-2 gap-4
+        md:flex md:flex-col md:items-end md:gap-4 2xl:gap-8
+        px-8 py-4
+        md:px-0 md:pt-[20vh] md:pb-0
+        "
+      >
+        <StatBlock value="3" label="Internships" />
+        <StatBlock value="18+" label="Hackathons" />
+        <StatBlock value="10+" label="Projects" />
+        <StatBlock value="6" label="Leadership roles" />
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,13 @@
-"use client";
-
-import { useState } from "react";
-
-import Header from "@/sections/Header/Header"; // Import Header
-import Hero from "@/sections/Hero/Hero";
-import About from "@/sections/About/About";
-// import Education from "@/sections/Education/Education";
-// import Experience from "@/sections/Experience/Experience";
-// import Skills from "@/sections/Skills/Skills";
-// import Projects from "@/sections/Projects/Projects";
-// import Testimonials from "./sections/Testimonials/Testimonials";
-// import Contact from "./sections/Contact/Contact";
-
-type Language = "English" | "Chinese" | "Malay";
+import Header from "./components/Header";
+import Hero from "./components/sections/Hero";
 
 export default function Home() {
-  const [language, setLanguage] = useState<Language>("English");
-
-  const toggleLanguage = () => {
-    setLanguage((prev) =>
-      prev === "English" ? "Chinese" : prev === "Chinese" ? "Malay" : "English"
-    );
-  };
-
   return (
     <>
-      {/* Use Header Component */}
-      <Header language={language} toggleLanguage={toggleLanguage} />
-
-      <main
-        id="scroll-container"
-        className="flex flex-col w-full h-full snap-y snap-mandatory scroll-smooth gap-[5vh] overlow-x-hidden dark:bg-[#242424]"
-      >
-        <Hero language={language} />
-
-        <About language={language} />
-
-        {/* <Education /> */}
-
-        {/* <Experience /> */}
-
-        {/* <Skills /> */}
-
-        {/* <Projects /> */}
-
-        {/* <Testimonials language={language} /> */}
-
-        {/* <Contact /> */}
+      <Header />
+      <main className="w-full h-full">
+        <Hero />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </>
   );
 }
