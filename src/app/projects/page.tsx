f0cb6d21183adcdc4dd7ddefa2f03aeeb6072254
Edmund Lin <72682413+elz-ming@users.noreply.github.com>
2025-08-05 02:01:30 +0800

    002 | Create Projects sections (#3)
    * feat: create projects display
    
    * feat: create hyperlinks
    
    * feat: add youtube links

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import Projects from "@/app/components/sections/Projects";
+
+export default function Home() {
+  return (
+    <main className="w-full h-full">
+      <Projects />
+    </main>
+  );
+}
